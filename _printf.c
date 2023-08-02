aee9f456f80803bc49d4b15341d83dc353e9aed0
Sequence-10 <habz3963@gmail.com>
2023-08-02 11:04:14 -0700

    C-Language PrintF Project Task 0/15

--- _printf.c
+++ _printf.c
@@ -8,30 +8,100 @@
 
 int _printf(const char *format, ...)
 {
+	char *str;
+	unsigned long int i, count = 0;
 	va_list args;
-	int i = 0, j = 0;
-
-	if (format == 0)
-		return (-1);
-	if (format[0] == '%' && format[1] == '\0')
-		return (-1);
 
 	va_start(args, format);
-
-	while (format[i] != '\0')
+	if (format)
 	{
-		if (format[i] == '%' && format[i + 1] != '\0')
-			j += process_flag(format[i + 1], args);
-		if (!is_flag(format[i], format[i + 1]))
+		for (i = 0; i < strlen(format); i++)
 		{
-			j += _putchar(format[i]);
-			i++;
+			if (format[i] == '%')
+			{
+				i++;
+				if (format[i] == 'c')
+				{
+					putchar(va_arg(args, int));
+					count += 1;
+				}
+				else if (format[i] == 's')
+				{
+					str = va_arg(args, char *);
+					count += print_str(str);
+				}
+				else if (format[i] == '%')
+				{
+					putchar('%');
+					count += 1;
+				}
+				else if (format[i] == 'b')
+				{
+					unsigned int n = va_arg(list, unsigned int);
+					int bits = 0;
+					unsigned int mask = 1;
+
+					while (mask <= n)
+					{
+						bits++;
+						mask <<= 1;
+					}
+					if (bits == 0)
+					{
+						putchar('0');
+					}
+					else
+					{
+						mask >>= 1;
+						while (mask)
+						{
+							putchar((n & mask) ? '1' : '0');
+							mask >>= 1;
+						}
+					}
+				}
+				else if (format[i] == 'd' || format[i] == 'i')
+					count += printf("%d", va_arg(args, int));
+				else if (format[i] == 'u')
+					count += printf("%u", va_arg(args, int));
+				else if (format[i] == 'o')
+					count += printf("%o", va_arg(args, int));
+				else if (format[i] == 'x')
+					count += printf("%x", va_arg(args, int));
+				else if (format[i] == 'X')
+					count += printf("%X", va_arg(args, int));
+				else if (format[i] == 'p')
+					count += printf("%p", va_arg(args, void *));
+				else if (format[i] == 'S')
+				{
+					str = va_arg(args, char *);
+					count += handle_S(str);
+				}
+				else if(format[i] == 'r')
+				{
+					str = va_arg(args, char *);
+					count += print_rev(str);
+				}
+				else if (format[i] == '\0')
+					return (-1);
+
+				else
+				{
+					putchar(format[i - 1]);
+					putchar(format[i]);
+					count += 2;
+				}
+			}
+			else
+			{
+				putchar(format[i]);
+				count += 1;
+			}
 		}
-		else
-			i += 2;
+		va_end(args);
 	}
+	else
+		return (-1);
 
-	va_end(args);
-	return (j);
-
+	return (count);
 }
--- main.h
+++ main.h
@@ -5,14 +5,15 @@
 #include <string.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-int process_flag(char specifer, va_list args);
-int is_flag(char percent, char specifier);
-int print_char(va_list args);
 int _puts(char *string);
 int _puts_digit(int num);
 int _print_address(va_list args);
+int print_rev(char *s);
+int print_str(char *str);
+int handle_S(char *str);
 
 #endif
