69107724a0fa01b3de89474b45945efa22787a27
Sequence-10 <habz3963@gmail.com>
2023-08-02 13:17:53 -0700

    C-Language PrintF Project Task 0/15

--- _printf.c
+++ _printf.c
@@ -2,6 +2,7 @@
 int print_rev(char *s);
 int print_str(char *str);
 int handle_S(char *str);
+int print_str(char *str);
 
 /**
  * _printf - printing whatever is given as an input GIGO
@@ -197,5 +198,90 @@ int print_rev(char *s)
 			i--;
 		}
 	}
+	return (count);
+}
+
+if (!format)
+	return (-1);
+
+	char *str;
+	unsigned long int i, count = 0;
+	va_list list;
+
+	va_start(list, format);
+if (format)
+{
+	for (i = 0; i < strlen(format); i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				putchar(va_arg(list, int));
+				count += 1;
+			}
+			else if (format[i] == 's')
+			{
+				str = va_arg(list, char *);
+				count += print_str(str);
+			}
+			else if (format[i] == '%')
+			{
+				putchar('%');
+				count += 1;
+			}
+			else if (format[i + 1] == '\0')
+				return (-1);
+
+			else
+			{
+				putchar(format[i - 1]);
+				putchar(format[i]);
+				count += 2;
+			}
+		}
+		else
+		{
+			putchar(format[i]);
+			count += 1;
+		}
+	}
+	va_end(list);
+}
+else
+return (-1);
+
+return (count);
+}
+
+/**
+ * print_str - print the string
+ * @str: string to be printed
+ *
+ * Return: number of characters printed
+ */
+int print_str(char *str)
+{
+	unsigned long int j, count = 0;
+
+	if (str)
+	{
+		for (j = 0; j < strlen(str); j++)
+		{
+			putchar(str[j]);
+			count += 1;
+		}
+	}
+	else
+	{
+		str = "(null)";
+		for (j = 0; j < strlen(str); j++)
+		{
+			putchar(str[j]);
+			count += 1;
+		}
+	}
+
 	return (count);
 }
